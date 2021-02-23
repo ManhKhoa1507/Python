e567b5b61c15e3f150f9039822daef1bab460fbb
ManhKhoa1507 <nguyenlemanhkhoa15072001@gmail.com>
2021-02-23 09:40:01 +0700

    add rabbit_hole.py

--- rabbit_hole.py
+++ rabbit_hole.py
@@ -1,35 +1,37 @@
 import requests
-import re
+import csv
 
+def get_page(text) :
+    r = text.split()
+    page = r[len(r) - 1]
+    return page 
 
-def get_key(text) : 
-    r = re.split("\s",text)
-    key = r[len(r) - 1] 
-    return key
-
-def get_response(url,i) :
-    new_url = url+key
+def get_array(text) :
+    r = text.split()
+    
+    first_number = r[0].replace("[","").replace(",","")
+    second_number = r[1].split("\'")[1]
+   
+    array = [first_number, second_number]
+    return array 
+    
+def get_response(url,page) :
+    new_url = url+page
     response = requests.get(new_url)
     return response
-
 
 if __name__ == '__main__' :
 
     url = 'http://167.71.246.232:8080/rabbit_hole.php?page='
-    key = '4O48APmBiNJhZBfTWMzD'
+    page = 'cE4g5bWZtYCuovEgYSO1'
+    response = get_response(url, page)
     
-    n = int(input())
-    for i in range(n) : 
-        response = get_response(url, key)
-        
-        if(response.status_code == 200) :
-            key = get_key(response.text)
-            print(i,':   ',key)
-            
-            if(key == 'cE4g5bWZtYCuovEgYS01') :
-                tmp = repsonse.text
-                break;
-                print(tmp)
-        else : 
-            print('Not response')
+    with open('rabbit_hole.csv', 'w', newline = '') as csvfile :
+        rabbit_csv = csv.writer(csvfile, delimiter=';', quotechar='|', quoting = csv.QUOTE_MINIMAL)
+    
+        while(response.text != 'end') :
+            page = get_page(response.text)
+            array = get_array(response.text)
+            response = get_response(url, page)
 
+            rabbit_csv.writerow(array)
